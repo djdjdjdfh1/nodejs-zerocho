cc84419418de74a15f48fa644c3158e9d26c2f01
Park Chan <qkrcks8028@gmail.com>
2025-02-24 18:39:16 +0900

    node서버 구축

--- ch4/4.1/createServer.js
+++ ch4/4.1/createServer.js
@@ -2,4 +2,9 @@ const http = require('http');
 
 http.createServer((req, res) => {
   // 여기에 어떻게 응답할 지 적어줍니다.
+  res.write('<h1>Hello Node!</h1>');
+  res.write('<p>Hello Server!</p>');
+  res.end('<p>Hello</p>');
+}).listen(8080, () => {
+  console.log('8080번 포트')
 });
