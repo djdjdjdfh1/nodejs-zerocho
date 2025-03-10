da3c9b7a05a0db1c1326987de98b52137699fc2c
Park Chan <qkrcks8028@gmail.com>
2025-03-10 19:53:15 +0900

    query 공부

--- ch7/7.6/learn-sequelize/app.js
+++ ch7/7.6/learn-sequelize/app.js
@@ -20,7 +20,7 @@ sequelize.sync({ force: false })
     console.log('데이터베이스 연결 성공');
   })
   .catch((err) => {
-    console.error('err: ', err);
+    console.error(err);
   });
 
 app.use(morgan('dev'));
