701f84b129cd46dece4e17c50ac613e73f5f22f1
Park Chan <qkrcks8028@gmail.com>
2025-03-19 18:15:49 +0900

    node 학습

--- ch12/12.6/gif-chat/schemas/index.js
+++ ch12/12.6/gif-chat/schemas/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { MONGO_ID, MONGO_PASSWORD, NODE_ENV } = process.env;
 const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/admin`;
 
-console.log('몽고DB', MONGO_URL);
+console.log(MONGO_URL);
 
 const connect = () => {
   if (NODE_ENV !== 'production') {
